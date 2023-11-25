46f7bc62256e8807a2d56ae170d9f91a1e9af67e
Nomu1Nomu1 <isaiah.tino@gmail.com>
2023-11-25 23:12:31 +0700

    Tabel data mahasiswa

--- index.php
+++ index.php
@@ -32,8 +32,10 @@
                 aria-labelledby="offcanvasNavbarLabel">
                 <!-- Sidebar header -->
                 <div class="offcanvas-header text-white border-bottom">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Informasi Akademik</h5>
+                    <button type="button" class="btn-close btn-close-white shadow-none" data-bs-dismiss="offcanvas"
+                        aria-label="Close"></button>
+                    <!-- Sidebar body -->
                 </div>
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
@@ -41,27 +43,50 @@
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="#">Mahasiswa </a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown"
                                 aria-expanded="false">
-                                Dropdown
+                                Profile
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item" href="#"><i class="uil uil-signin"></i>Login</i></a></li>
                             </ul>
                         </li>
+                        <img src="image/Aqua Minato.jpg" alt="Logo" class="image_profile justify-content-end" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
                     </ul>
                 </div>
             </div>
         </div>
     </nav>
+
+    <div class="container mt-5">
+        <h2 class="title text-center mb-5">Data Mahasiswa</h2>
+        <table class="table table-striped mt-3">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NIM</th>
+                    <th>Nama</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Jurusan</th>
+                    <th>Alamat</th>
+                    <th>ID Wali</th>
+                </tr>
+            </thead>
+            <tbody>
+                <td>37255</td>
+                <td>1526492378</td>
+                <td>Nomu Nomu</td>
+                <td>Laki-Laki</td>
+                <td>Informatika</td>
+                <td>Klaruan, Palur, RT 03 RW 15, Mojolaban</td>
+                <td>2</td>
+            </tbody>
+        </table>
+    </div>
 
 </body>
 
